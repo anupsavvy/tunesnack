70653068bb5e8e7f11f8112e0e18a84b298a8aad
anupsawant <anup.sawant@northwestern.edu>
2017-03-21 22:50:30 -0500

    first commit

---
+++ js/chat.js
@@ -0,0 +1,4 @@
+/*
+	@desc: This file has the logic for all the chat functionality of 
+	tunesnack
+*/
---
+++ js/music.js
@@ -0,0 +1,3 @@
+/*
+	@desc: This file has all the functionality for playing music in chat room.
+*/
