1250f8b7aeb92282cae51825d89981399b42f73a
Mason <MasonJinLuo@gmail.com>
2017-03-28 18:23:11 -0500

    working chat system, NOT working username, working AJAX robot pictures

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,6 @@
 /*
-	@desc: This file has the logic for all the chat functionality of 
-	tunesnack
+  @desc: This file has the logic for all the chat functionality of 
+  tunesnack
 */
  
 
@@ -21,6 +21,7 @@ firebase.initializeApp(config);
 
 
 var database = firebase.database();
+var usernameDatabase = database.ref("/username")
 var popChatDatabase = database.ref("/popchat");
 var rockChatDatabase = database.ref("/rockchat");
 var indieChatDatabase = database.ref("/indiechat");
@@ -33,52 +34,64 @@ var current_genre = "";
 
 
 $('#enter').on('click', function(){
+popChatDatabase.off('child_added');
+rockChatDatabase.off('child_added');
+indieChatDatabase.off('child_added');
+electricChatDatabase.off('child_added');
+countryChatDatabase.off('child_added');
 
 $('#Messages').empty();
+setUsername()
 
-var selectedGenre = $('#username option:selected')[0].value;
+var selectedGenre = $('#genreoptions option:selected')[0].value;
 console.log(selectedGenre);
 
 if (selectedGenre === "pop"){
   
-  localStorage.setItem('genre', 'pop')
-  console.log("pop")
+  localStorage.setItem('genre', 'pop');
+  generateRobot();
+  console.log("pop");
 
 
 }
 
 if (selectedGenre === "rock"){
   
-  localStorage.setItem('genre', 'rock')
-  console.log("rock")
+  localStorage.setItem('genre', 'rock');
+  generateRobot();
+  console.log("rock");
+
 
 }
 
 if (selectedGenre === "indie"){
   
-  localStorage.setItem('genre', 'indie')
-  console.log("indie")
+  localStorage.setItem('genre', 'indie');
+  generateRobot();
+  console.log("indie");
 
 }
 
 if (selectedGenre === "electric"){
   
-  localStorage.setItem('genre', 'electric')
-  console.log("electric")
+  localStorage.setItem('genre', 'electric');
+  generateRobot();
+  console.log("electric");
 
 }
 
 if (selectedGenre === "country"){
 
-  localStorage.setItem('genre', 'country')
-  console.log("country")
+  localStorage.setItem('genre', 'country');
+  generateRobot();
+  console.log("country");
 
 }
 
 
 // continue to work here
 
-console.log($('#username option:selected')[0].value);
+console.log($('#genreoptions option:selected')[0].value);
 
 
 if(localStorage.getItem("genre") === "pop"){
@@ -123,28 +136,98 @@ if(localStorage.getItem("genre") === "country"){
 
 }
 
+});
+
+function setUsername() {
+input = $('#username').val().toString().toLowerCase();
+  usernameDatabase.once('value', function(snapshot) {
+  var users = snapshot.val();
+ 
+    for(user in users){
+
+      console.log(users[user]);
+      if(users[user].toString().toLowerCase() === input){
+        alert('try another name');
+        return false;
+      }else{
+          usernameDatabase.push(input);
+          localStorage.setItem('username', username);
+          return false;
+
+      }
+    }
+
+  });
+//   localStorage.setItem('username', $('#username').val());
+}
+
+
+function generateRobot (){
+// username = localstorage.getItem('username')
+var queryURL = 'https://robohash.p.mashape.com/index.php?text='+ username;
+
+
+$.ajax({
+    url:  queryURL, // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+    dataType: 'json',
+    success: function(data) {
+
+    console.log((data.imageUrl)); 
+    var imageURL = data.imageUrl;
+    localStorage.setItem("imageURL" , imageURL);
+
+  },
+    error: function(err) { console.log(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "0q6E1TrM3zmshpYCK0rIO60DmVyEp1McPMkjsn8CXxBDRmjxLY"); // Enter here your Mashape key
+    }
+
+});
+
+
+};
+
 
 
 function getMessageAppend(messageObj){
 
   console.log(messageObj.val())
   convertedTime = moment.unix(messageObj.val().time).format('LT')
-  var p = $('<p>')
-  newMessage = messageObj.val().message
-  messageToAppend = p.text(messageObj.val().name + "(" + convertedTime + "): " +  messageObj.val().message);
-  $('#Messages').append( messageToAppend);
+  var newDiv = $('<div>');
+  var img = $('<img>')
+      img.attr('src', messageObj.val().picture);
+      img.attr('width' , '50px');
+      img.attr('height' , '50px');
+  $('#Messages').append(newDiv);
+  newDiv.append(img);
+  newDiv.append(messageObj.val().name);
+  newDiv.append("(" + convertedTime + "): ");
+  newDiv.append(messageObj.val().message);
+
+  // var img = $('<img>')
+  //     img.attr('src', messageObj.val().picture)
+  //     img.attr('width' , '50px')
+  //     img.attr('height' , '50px')
+  // var p = $('<p>')
+  // newMessage = messageObj.val().message
+  // messageToAppend = p.html(img + messageObj.val().name + "(" + convertedTime + "): " +  messageObj.val().message);
+  // $('#Messages').append( messageToAppend);
 
 }
 
 // send messages to the proper database locations
 
-$("#chat-send").click(function() {
+$("#chat-send").on('click' , function() {
+  var imagelink = localStorage.getItem('imageURL');
     if ($("#chat-input").val() !== "") {
       var message = $("#chat-input").val();
 
       if(localStorage.getItem("genre") === "pop"){
 
         popChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -156,6 +239,7 @@ $("#chat-send").click(function() {
       if(localStorage.getItem("genre") === "rock"){
 
         rockChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -167,6 +251,7 @@ $("#chat-send").click(function() {
       if(localStorage.getItem("genre") === "indie"){
 
         indieChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -178,6 +263,7 @@ $("#chat-send").click(function() {
       if(localStorage.getItem("genre") === "electric"){
 
         electricChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -189,6 +275,7 @@ $("#chat-send").click(function() {
       if(localStorage.getItem("genre") === "country"){
 
         countryChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -204,6 +291,7 @@ $("#chat-send").click(function() {
 });
 
 $("#chat-input").keypress(function(enter) {
+  imagelink = localStorage.getItem('imageURL');
 
     if (enter.keyCode === 13 && $("#chat-input").val() !== "") {
       var message = $("#chat-input").val();
@@ -211,6 +299,7 @@ $("#chat-input").keypress(function(enter) {
             if(localStorage.getItem("genre") === "pop"){
 
         popChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -223,6 +312,7 @@ $("#chat-input").keypress(function(enter) {
       if(localStorage.getItem("genre") === "rock"){
 
         rockChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -235,6 +325,7 @@ $("#chat-input").keypress(function(enter) {
       if(localStorage.getItem("genre") === "indie"){
 
         indieChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -247,6 +338,7 @@ $("#chat-input").keypress(function(enter) {
       if(localStorage.getItem("genre") === "electric"){
 
         electricChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -259,6 +351,7 @@ $("#chat-input").keypress(function(enter) {
       if(localStorage.getItem("genre") === "country"){
 
         countryChatDatabase.push({
+          picture: imagelink,
           name: username,
           message: message,
           time: firebase.database.ServerValue.TIMESTAMP,
@@ -272,14 +365,4 @@ $("#chat-input").keypress(function(enter) {
 
     
 });
-
-
-
-
-
-
-});
-
-
-
 
