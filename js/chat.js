35432ccbd772e4ebb23bee3291af8524a8746ba4
anupsawant <anup.sawant@northwestern.edu>
2017-03-31 12:49:16 -0500

    changes to js and index page

--- js/chat.js
+++ js/chat.js
@@ -22,11 +22,51 @@ var username = 'guest';
 var message;
 var current_genre = "";
 var loading_messages = [
-    "You are awsome !!!!"
-    , "Have a wonderful day ahead !!!"
+    "You are awesome !!"
+    , "Have a wonderful day !!"
     , "Life is good !!"
-    , "Work hard and take care of yourself!!"
+    , "Work hard, play hard !!"
+    , "Keep loving bootcamp !!"
+    , "Thank you Steven, N8 and all other TA's !!"
 ]
+var play_radio = function (genre) {
+    var station = "";
+    if (genre === 'rock') {
+        station = 'absoluterockradio';
+    }
+    else if (genre === 'indie') {
+        station = 'theindieblend';
+    }
+    else if (genre === 'electric') {
+        station = '-radiopartypl-house-discohouse-progressive-edm';
+    }
+    else if (genre === 'country') {
+        station = '113fmbigkickin-country';
+    }
+    else if (genre === 'pop') {
+        station = 'absoluterockradio';
+    }
+    (function (win, doc, script, source, objectName) {
+        (win.RadionomyPlayerObject = win.RadionomyPlayerObject || []).push(objectName);
+        win[objectName] = win[objectName] || function (k, v) {
+            (win[objectName].parameters = win[objectName].parameters || {
+                src: source
+                , version: '1.1'
+            })[k] = v;
+        };
+        var js, rjs = doc.getElementsByTagName(script)[0];
+        js = doc.createElement(script);
+        js.async = 1;
+        js.src = source;
+        rjs.parentNode.insertBefore(js, rjs);
+    }(window, document, 'script', 'https://www.radionomy.com/js/radionomy.player.js', 'radplayer'));
+    radplayer('url', station);
+    radplayer('type', 'mobile');
+    radplayer('autoplay', '1');
+    radplayer('volume', '50');
+    radplayer('color1', '#000000');
+    radplayer('color2', '#ffffff');
+}
 var handle_chat = function (selectedGenre) {
     popChatDatabase.off('child_added');
     rockChatDatabase.off('child_added');
@@ -64,17 +104,35 @@ var handle_chat = function (selectedGenre) {
 }
 $('#enter').on('click', function (e) {
     e.preventDefault();
+    $("#warning").fadeOut('slow');
     setUsername();
 });
 $(".channel").click(function () {
     handle_chat($(this).attr('data-channel'));
 });
+var refresh_chat_room = function (genre) {
+    var icon = "#" + localStorage.getItem('genre') + '-icon';
+    var num = Math.floor((Math.random() * 5) + 1);
+    $("#loading-message").html(loading_messages[num]);
+    setTimeout("$('#cool-message').fadeIn('slow');", 500);
+    setTimeout("$('#cool-message').fadeOut('slow');", 1000);
+    setTimeout("$('#chat-elements').fadeIn('slow');", 2000);
+    $("#name").html(localStorage.getItem('username'));
+    $(icon).removeClass('icofont-play-alt-3');
+    $(icon).addClass('icofont-song-notes');
+    //    handle_chat(localStorage.getItem('genre'));
+    handle_chat(genre);
+    setTimeout("play_radio(localStorage.getItem('genre'));", 3000);
+    setTimeout("$('body').find('iframe').css({'height': '0', 'width': '0', 'border': 'none'});", 4000);
+}
 
 function setUsername() {
+    var login = false;
     if (localStorage.getItem('username') !== null) { // this function checks if username exist in localstorage
         username = localStorage.getItem('username') //if it does, set username = to the username stored in localstorage
         setTimeout("$('#login-elements').fadeOut('slow');", 100);
         setTimeout("$('#chat-elements').fadeIn('slow');", 500);
+        login = true;
     }
     else { // if not check if the username already exist in firebase, for uniqueness
         input = $('#username').val().toString().toLowerCase();
@@ -83,19 +141,27 @@ function setUsername() {
             for (user in users) {
                 console.log(users[user]);
                 if (users[user].toString().toLowerCase() === input) { //if it does, alert try another name
-                    alert('try another name');
-                    return false
+                    $("#warning").fadeIn('slow');
+                    setTimeout("$('#warning-message').html('This username has already been taken.Try a new name.')", 100);
+                    return false;
+                }
+                else if (input === "") {
+                    $("#warning").fadeIn('slow');
+                    setTimeout("$('#warning-message').html('Username cannot be blank.')", 100);
+                    return false;
                 }
             }
-            alert('push to database') //if it doesnt, it pushes the name to firebase and save the username in localstorage
             usernameDatabase.push(input)
             localStorage.setItem('username', input);
+            login = true;
         });
     }
-    var selectedGenre = $('#genreoptions option:selected')[0].value;
-    handle_chat(selectedGenre);
-    setTimeout("$('#login-elements').fadeOut('slow');", 100);
-    setTimeout("$('#chat-elements').fadeIn('slow');", 500);
+    if (login === true) {
+        var selectedGenre = $('#genreoptions option:selected')[0].value;
+        setTimeout("$('#login-elements').fadeOut('slow');", 100);
+        //        setTimeout("$('#chat-elements').fadeIn('slow');", 500);
+        refresh_chat_room(selectedGenre);
+    }
 }
 
 function generateRobot() {
@@ -121,7 +187,7 @@ function generateRobot() {
 
 function getMessageAppend(messageObj) {
     convertedTime = moment(messageObj.val().time).format('LT')
-    $("#chat-list").append("<li><div class='chat-body'><div class='row'><div class='col-sm-1'><img class='img-circle' src='" + messageObj.val().picture + "'></div><div class='col-sm-11'><strong class='pull-left'><span><i class='icofont icofont-chat'></i></span> " + messageObj.val().name + "</strong><small class='text-muted'><span class='glyphicon glyphicon-time'>  </span>" + convertedTime + "</small><div class='row'><div class='col-sm-12'><p id='chat-content'>" + messageObj.val().message + "</p></div></div></div></div></div></li>");
+    $("#chat-list").append("<li><div class='chat-body'><div class='row'><div class='col-sm-1'><img class='img-circle' src='" + messageObj.val().picture + "'></div><div class='col-sm-11'><strong class='pull-left'>" + messageObj.val().name + "</strong><small class='text-muted'><span class='glyphicon glyphicon-time'>  </span>" + convertedTime + "</small><div class='row'><div class='col-sm-12'><p id='chat-content'>" + messageObj.val().message + "</p></div></div></div></div></div></li>");
     var chat_panel = $('#chat-panel');
     var height = chat_panel[0].scrollHeight;
     chat_panel.scrollTop(height);
